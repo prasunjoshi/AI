1cdcdf020ce04a13a80a10fe2f9413eb0be522ea
itachi <psuryapratap24@gmail.com>
2018-12-18 19:11:43 +0530

    final commit with

--- AI_pyfiles/import os_H.py
+++ AI_pyfiles/import os_H.py
@@ -1,3 +1,5 @@
+# This file is used to implement the actual code of a 100 linearly increasig value of the bank account limit.
+
 import  v3_allstages_H_plus_opt as code
 while(1):
 	code.setL()
@@ -8,5 +10,5 @@ while(1):
 		print("error",e)
 	code.writetocsv()
 	print(":::::::::::::::::::::::::::::::::::::::::::::::::::::",code.counts())
-	if(code.counts()>100):
+	if(code.counts()>99):
 		break
--- AI_pyfiles/v3_allstages_H_plus_opt.py
+++ AI_pyfiles/v3_allstages_H_plus_opt.py
@@ -7,23 +7,56 @@ import random
 import csv,os
 import pandas as pd
 
-T=4
-N=10
 
-V=[]
-B=[[0 for b in range (N)]for t in range (T)]
-Z=[[0 for b in range (N)]for t in range (T)]
-Q=[[0 for b in range (N)]for t in range (T)]
-val=[[0 for b in range (N)]for t in range (T)]
-LB=[[0 for b in range (N)]for t in range (T)]
+T=4 # The total number of stages the auction is held on.
+N=10 # Number of buyers taking part. can be changes to have different buyers in each stage.
+V=[] # Valuations of each buyers. In this code taken from
 
-# bank account limit : random number (range to be decided) matrix of size TxB
+B={} #Variable to store bank balance for each buyer.
+
+Z={} #Indicates which buyer is assigned the product in each stage.
+
+Q={} #Variable to store Payment made by the buyers(who are alloted the product) in each stage.
+
+val={} # valt as in equation (19) in the paper reffered.
+
+LB={} # varaible as in equation (19) used to calculate the deposit amount.
+
+# all the above variables after V are calculated for two different types of auctions.
+
+#index 0 is for the standard double Reserve Auction (Mechanism 2 in section 3.)
+#index 1 is for HDR auction (Mechanism 3 in section 4.)
+B[0]=[[0 for b in range (N)]for t in range (T)]
+B[1]=[[0 for b in range (N)]for t in range (T)]
+
+
+Z[0]=[[0 for b in range (N)]for t in range (T)]
+Z[1]=[[0 for b in range (N)]for t in range (T)]
+
+
+Q[0]=[[0 for b in range (N)]for t in range (T)]
+Q[1]=[[0 for b in range (N)]for t in range (T)]
+
+
+val[0]=[[0 for b in range (N)]for t in range (T)]
+val[1]=[[0 for b in range (N)]for t in range (T)]
+
+
+LB[0]=[[0 for b in range (N)]for t in range (T)]
+LB[1]=[[0 for b in range (N)]for t in range (T)]
+
+# for every run of the code we call the fucntion setL to set the Bank Account Limit to a value 0.012 units
+# higher than the previous run. This is done so as to get a revenue v/s limit graph for both index 0 and index 1.
+# the value of both revenues are stored in a file w.r.t corresponding limit.
+
 def setL():
+	global l
 	if(os.path.exists("Plot_H.csv")==0):
 		writer=csv.writer(open('Plot_H.csv','w'))
-		writer.writerow(["Limit","HDR Revenue","Optimal Revenue"])
+		writer.writerow(["Limit","Mechanism 2 Revenue","HDR Revenue","Optimal Revenue","Efficiency M2","Efficiency HDR"])
 		l=-0.012
 	else:
+		l=0
 		filename="Plot_H.csv"
 		df=pd.read_csv(filename)
 		if (df.empty==0):
@@ -34,18 +67,11 @@ def setL():
 	global L
 	L=[[l+0.012 for b in range (N)]for t in range (T)]
 
-	# for b in range (10):
-	#     for t in range (4):
-	#         L[t][b]=l
-	#     l+=0.12
 	print("\nL ",L[0][0])
 
-# lower reserved value rt(0) range and values to be decided.
 
-#R=[1 for t in range (T)]
 
-# distribution function for valuation.. variable:x 
-# exponential distribution
+# function to ramdomly decide the Valuation of N buyers from an exponential distribution
 
 def valuations():
 		V.clear()
@@ -59,27 +85,33 @@ def valuations():
 
 
 ###########################################################################################################################
-############################## 1.FUNCTIONS FOR MECHANISM 2 OF THE DOUBLE RESERVE AUCTION ##################################
+######################## 1.FUNCTIONS FOR MECHANISM 2/MECHANISM 3 OF THE DOUBLE RESERVE AUCTION ###########################
 
 
 #integrand or 1-F(v) where F(v) is the distribution function.
+# This F(v) can be replaced by some other function which tells us an equation that defines how a given set of 
+# valuations are distributed.
 
 def integrand(x):
 	return 1-(math.exp(-x))
 
-# function to find the integration of the integrand from low to up.
+# function to find the integration of the integrand from lower limit to upper limit.
 
 def integVal(lowl,upl):
     val,err = scipy.integrate.quad(integrand,lowl,upl)
     return val
 
-# dynamic reserved value function
+
+
+# used to dynamically change the reserved value for the later stages as defined in section 3.
+
 def resVal(sp,t,b):
 	if(sp!=0):
 		return findrtnz(sp,storedRt0[t])
 	else:
 		return storedRt0[t]
 
+# dynamic reserved value function for HDR Mechanism 3.
 
 def resVal_H(sp,t,b):
 	if(sp!=0):
@@ -101,80 +133,86 @@ def findrtnz(sp,R):
 def solveforll(x,sp,R):
 	return si.quad(integrand, x, R)[0]-sp
 
+#for the functions below variable "fl" is a flag used to indicate Mechanism 2 and Mechanism 3.
+
 # allocattion Rule
 
 def allocationRule(V,B,t,fl):
 	for b in range(N):
-		if(fl==0):
-			if(V[b]>resVal(spendPolicy(B,t,b),t,b)):
-			Z[t][b]=1
-		else:
-			if(V[b]>resVal_H(spendPolicy(B,t,b),t,b)):
-			Z[t][b]=1
-	print("allocation at stage ",t," : ",Z[t])
+		if(V[b]>resVal(spendPolicy(B,t,b,fl),t,b)):
+			Z[fl][t][b]=1
+	print("allocation at stage0 ",t+1," : ",Z[fl][t])
+
+# deciding the payment value each stage each mechanism each buyer
 
 def paymentRule(V,Z,B,t,fl):	
 	for b in range(N):
-		if(fl==0):
-			Q[t][b]=Z[t][b]*resVal(spendPolicy(B,t,b),t,b)
-		else:
-			Q[t][b]=Z[t][b]*resVal_H(spendPolicy(B,t,b),t,b)
-	print("payment rule at stage ",t," : ",Q[t])
+		Q[fl][t][b]=Z[fl][t][b]*resVal(spendPolicy(B,t,b,fl),t,b)
+	print("payment rule at stage ",t," : ",Q[fl][t])
 
-def spendPolicy(B,t,b):
-	return B[t][b]
+# deciding the spend policy according to the definition of DR.
+
+def spendPolicy(B,t,b,fl):
+	return B[fl][t][b]
+
+# set the amount to be deposited to the bank account of every buyer at the end of each stage using equation (19)
 
 def depositPolicy(Z,B,L,t,fl):
 	for b in range(N):
-		if(fl==0):
-			val[t+1][b]=integVal(0,resVal(0,t,b))
+		val[fl][t+1][b]=integVal(0,resVal(0,t,b))
 
-			LB[t+1][b]=min(L[t+1][b],val[t+1][b])
+		LB[fl][t+1][b]=min(L[t+1][b],val[fl][t+1][b])
 
-			D=Z[t][b]*LB[t+1][b]
+		D=Z[fl][t][b]*LB[fl][t+1][b]
 
-			B[t+1][b]=B[t][b]+D-spendPolicy(B,t,b)
-		else:
-			val[t+1][b]=integVal(0,resVal(0,t,b))
+		B[fl][t+1][b]=B[fl][t][b]+D-spendPolicy(B,t,b,fl)
+	print ("Balance after deposit for stage ", t," : ", B[fl][t])
+	print ("L bar after deposit ", LB[fl])
 
-			LB[t+1][b]=min(L[t+1][b],val[t+1][b])
-
-			D=Z[t][b]*LB[t+1][b]
-
-			B[t+1][b]=B[t][b]+D-spendPolicy(B,t,b)
-	print ("Balance after deposit for stage ", t," : ", B[t])
-	print ("L bar after deposit ", LB)
-
+# the below variable stores the initial reserved price, mentioned as rt(0) in the paper, for each stage.
 storedRt0=[]
 
 def doubleReserve():
-	global sm
-	sm=0
+	global sm0
+	global sm1
+	# below two variables are used to store the maximum revenue for each mechanisms.
+	sm0=0
+	sm1=0
 	storedRt0.clear()
 	for t in range (T):
 		print("\nStage ",t+1,"\n")
 		V=valuations()
-		# R=random.uniform(min(V),max(V))
+
+		# reserved price calculated as average of all the valuations plus half of the max valuation.
+		# this is done so as to make sure that some values are there in the randomly selected valuations
+		# which pass the initial allocation. 
+		# For a well defined dataset R will have a proper value depending on the product and for every stage.
+
 		R=sum(V)/len(V)+max(V)/2
 		storedRt0.append(R)
 		print("R ",R)
-		allocationRule(V,B,t)
-		paymentRule(V,Z,B,t)
+		allocationRule(V,B,t,0)
+		allocationRule(V,B,t,1)
+		paymentRule(V,Z,B,t,0)
+		paymentRule(V,Z,B,t,1)
 		if(t!=T-1):
-			depositPolicy(Z,B,L,t)
-		sm+=max(Q[t])
-	print ("MAX revenue",sm)
+			depositPolicy(Z,B,L,t,0)
+			depositPolicy(Z,B,L,t,1)
+		sm0+=max(Q[0][t])
+		sm1+=max(Q[1][t])
+	print ("Mechanism 2 Revenue ",sm0)
+	print ("HDR Revenue (Mechanism 3) ",sm1)
 
 
 
-
+## explaination of the above double reserve function.
 # run loop for t stages calling the respective functions.
 # for 1 to T :
 # call valuations.
 # call allocation rule.
 # call payment rule.
 # call deposit policy.
-# calculate the total revenue that is generated. (need to discuss on how it is to be done)
+# calculate the total revenue that is generated.
 
 
 #####################################################################################################################
@@ -184,27 +222,25 @@ def doubleReserve():
 #####################################################################################################################
 ##################################### 2.OPTIMAL REVENUE CALCULATION. ################################################
 
-
-
-
+# This part implements section 3.2 which gives the optimal revenue which is h[0], i.e from stage 0 to T.
 
 
 alp=[[0 for b in range (N)]for t in range (T)]
 
 def alphdet(B,LB,alp):
 	for b in range (0,N):
-		if(B[1][b]==LB[1][b]):  ###  LB[b][t]=min(L[b][t],val[b][t])
+		if(B[0][1][b]==LB[0][1][b]):  ###  LB[b][t]=min(L[b][t],val[b][t])
 			alp[1][b]=1
 		else:
 			alp[1][b]=0
 		for t in range (2,T-1):
-			alp[t+1][b]=1-alp[t][b]*math.exp(-1*resVal(LB[t][b],t,b))-(1-alp[t][b])*math.exp(-1*resVal(0,t,b))
+			alp[t+1][b]=1-alp[t][b]*math.exp(-1*resVal(LB[0][t][b],t,b))-(1-alp[t][b])*math.exp(-1*resVal(0,t,b))
 
 def rhoh(u):
 	return u*(1-math.exp(-u))
 
 def Kt(alp,t,b,storedRt0):
-	kt=alp[t][b]*(rhoh(resVal(LB[t][b],t,b))-rhoh(storedRt0[t])+integVal(resVal(LB[t][b],t,b),storedRt0[t]))
+	kt=alp[t][b]*(rhoh(resVal(LB[0][t][b],t,b))-rhoh(storedRt0[t])+integVal(resVal(LB[0][t][b],t,b),storedRt0[t]))
 	return kt
 
 H=[[0 for b in range (N)]for t in range (T)]
@@ -217,22 +253,39 @@ def esrev(storedRt0,alp):
 		for t in range (T-2,-1,-1):
 			H[t][b]=Kt(alp,t,b,storedRt0)+rhoh(rt0)+H[t+1][b]
 
+#The function below is used for calculation of h[0] for the buyer who provides maximum value.
+
 def optimal():
 	alphdet(B,LB,alp)
 	esrev(storedRt0,alp)
 	print("Estimated revenue ", max(H[0]))
 
+#####################################################################################################################
+################################################# 2.END #############################################################
+
+####################################################################################################################
+################################### 3. Storing Limits and revenues accordingly.#####################################
+
 def writetocsv():
 	writer = csv.writer(open('Plot_H.csv', 'a'))
 	lt = float(str(L[0][0]).encode('utf-8'))
-	print("=============================",lt)
-	mxrev = float(str(sm).encode('utf-8'))
-	print("=============================",mxrev)
+
+	m2rev = float(str(sm0).encode('utf-8'))
+
+	m3rev = float(str(sm1).encode('utf-8'))
+
 	esrev = float(str(max(H[0])).encode('utf-8'))
-	print("=============================",esrev)
-	writer.writerow([lt,mxrev,esrev])
+
+	effm2 = float(str(sm0/max(H[0])).encode('utf-8'))
+
+	effhdr = float(str(sm1/max(H[0])).encode('utf-8'))
+
+	writer.writerow([lt,m2rev,m3rev,esrev,effm2,effhdr])
 
 def counts():
 	df=pd.read_csv("Plot_H.csv")
 	r=df.shape[0]
 	return r
+
+#####################################################################################################################
+################################################## EOC. #############################################################
